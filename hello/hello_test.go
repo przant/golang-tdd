9f991a310c3d2e2372a7a8984a9a37f8cde65e83
Antonio Pérez <apema2079@gmail.com>
2022-04-06 22:18:17 -0500

    Test French language param value

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -32,4 +32,11 @@ func TestHello(t *testing.T) {
 
 		assertCorrecMessage(t, got, want)
 	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
+
+		assertCorrecMessage(t, got, want)
+	})
 }
